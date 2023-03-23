9ae661c8bf6c31976b932ea8dda70bf1047ff51d
DavideAnile <davide.anile16@gmail.com>
2023-03-23 17:21:30 +0100

    feat :  end exercise + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,41 @@
+<script>
+
+</script>
+
+<template>
+
+<div class="jumbotron">
+    <h1>Gioco stupendo</h1>
+    <div class="img-container"><img src="../../public/images/it takes two.webp" alt=""></div>
+    <div class="description">Cody e May, una coppia sposata, decidono di divorziare in seguito a una discussione tra i due, nella quale Cody incolpa May di lavorare tutto il tempo, mentre May ribatte affermando che Cody non mostra il benché minimo apprezzamento per il suo lavoro. Dopo aver detto alla figlia Rose del divorzio, la bambina porta due bambole fatte a mano, con la forma dei genitori, nel capanno di famiglia. Si rivolge poi a un libro di terapia relazionale, intitolato "Book of Love" e scritto dal Dr. Hakim, chiedendogli una mano per risolvere la situazione. Inizia a giocare poi con le bambole, inscenando una riappacificazione tra i due genitori, e distrutta dalla situazione inizia a piangere. Subito dopo, i genitori si ritrovano intrappolati nei corpi delle bambole, a causa di una lacrima di Rose caduta su di esse. Il libro, che si fa chiamare semplicemente Dr. Hakim, dice a May e Cody che gli è stato affidato il compito di cercare di sistemare la loro relazione, mentre i due, ormai bambole, cercano di raggiungere Rose per ritornare com'erano prima.</div>
+</div>
+
+</template>
+
+<style>
+
+.jumbotron{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.jumbotron .description{
+    padding: 50px;
+}
+
+.jumbotron h1{
+    text-align: center;
+
+}
+
+.jumbotron .img-container{
+    width: 400px;
+    margin-top: 20px;
+}
+
+.jumbotron .img-container img{
+    width: 100%;
+}
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+    <div>
+        <h1>La mia prima App con Vite!</h1>
+    </div>
+
+</template>
+
+<style scoped>
+
+h1{
+    text-align: center;
+    color: red;
+    margin-bottom: 30px;
+}
+
+</style>
